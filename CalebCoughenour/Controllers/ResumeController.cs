dd42530a7bee96cc88dc34a242b7ed461c5f31c0
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-07-08 12:17:27 -0700

    add controller for resume index page

---
+++ CalebCoughenour/Controllers/ResumeController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalebCoughenour.Controllers
+{
+  public class ResumeController : Controller
+  {
+    public ActionResult Index()
+    {
+      return View();
+    }
+  }
+}
