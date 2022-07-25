1d66e07bb33453044a68056c260c33accda923fa
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-07-25 15:22:39 -0700

    restructure project so it could be deployed on Azure, might restructure again after playing around with files/directories

---
+++ Controllers/ProjectsController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalebCoughenour.Controllers
+{
+  public class ProjectsController : Controller
+  {
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+    public ActionResult AtchooDetails()
+    {
+      return View();
+    }
+    public ActionResult CryptoDetails()
+    {
+      return View();
+    }
+    public ActionResult SillystringzDetails()
+    {
+      return View();
+    }
+    public ActionResult PierresDetails()
+    {
+      return View();
+    }
+  }
+}
