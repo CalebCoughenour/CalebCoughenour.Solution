d2ff5d3deb5f3e40905d8e00c37a51537f308f7b
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-07-27 09:54:14 -0700

    add controller for faq

---
+++ Controllers/FAQController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalebCoughenour.Controllers
+{
+    public class FAQController : Controller
+    {
+      public ActionResult Index()
+      {
+        return View();
+      }
+
+    }
+}
