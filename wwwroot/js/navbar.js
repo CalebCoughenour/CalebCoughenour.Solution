32c0ad79c77cba0663ffdd573b124a263787633e
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-07-27 13:41:27 -0700

    change code to control navbar hiding when scrolling up

--- wwwroot/js/navbar.js
+++ wwwroot/js/navbar.js
@@ -1,8 +1,10 @@
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("myTopnav").style.top = "0";
   } else {
-    x.className = "topnav";
+    document.getElementById("myTopnav").style.top = "-50px";
   }
+  prevScrollpos = currentScrollPos;
 }
